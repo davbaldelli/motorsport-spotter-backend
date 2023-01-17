cda96f5da434971ae5c56ea26aa1740764d6bbd7
Davide Baldelli <davbaldelli@gmail.com>
2023-01-17 13:09:27 +0100

    removed hardcoded credentials

--- Main.go
+++ Main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
-	http2 "motorsportspotter.backend/controllers/http"
+	"motorsportspotter.backend/controllers/http"
 	repo "motorsportspotter.backend/repositories/mysql"
+	"os"
 )
 
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Host     string `json:"host"`
+}
+
 func main() {
-	dsn := fmt.Sprintf("%v:%v@tcp(%v:3306)/%v?charset=utf8mb4&loc=Local", "root", "SP589a%6", "api.acmodrepository.com", "motorsport_spotter")
+
+	var cred Credentials
+
+	if jsonFile, err := os.ReadFile("credentials.json"); err != nil {
+		log.Fatal("no credentials file")
+	} else {
+		if err := json.Unmarshal(jsonFile, &cred); err != nil {
+			log.Fatal("err parsing json")
+		}
+	}
+
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:3306)/%v?charset=utf8mb4&loc=Local", cred.Username, cred.Password, cred.Host, "motorsport_spotter")
 	dbase, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
@@ -19,12 +38,12 @@ func main() {
 		log.Print("Connected to database successfully")
 	}
 
-	router := http2.Web{
-		ChampCtrl:    http2.ChampionshipControllerImpl{Repo: repo.ChampionshipRepoImpl{Db: dbase}},
-		TracksCtrl:   http2.TracksControllerImpl{Repo: repo.TracksRepositoryImpl{Db: dbase}},
-		EventCtrl:    http2.EventControllerImpl{Repo: repo.EventsRepositoryImpl{Db: dbase}},
-		SessionsCtrl: http2.SessionsControllerImpl{Repo: repo.SessionRepositoryImpl{Db: dbase}},
-		NewsCtrl:     http2.NewsControllerImpl{Repo: repo.NewsRepositoryImpl{Db: dbase}},
+	router := http.Web{
+		ChampCtrl:    http.ChampionshipControllerImpl{Repo: repo.ChampionshipRepoImpl{Db: dbase}},
+		TracksCtrl:   http.TracksControllerImpl{Repo: repo.TracksRepositoryImpl{Db: dbase}},
+		EventCtrl:    http.EventControllerImpl{Repo: repo.EventsRepositoryImpl{Db: dbase}},
+		SessionsCtrl: http.SessionsControllerImpl{Repo: repo.SessionRepositoryImpl{Db: dbase}},
+		NewsCtrl:     http.NewsControllerImpl{Repo: repo.NewsRepositoryImpl{Db: dbase}},
 	}
 
 	router.Listen()
